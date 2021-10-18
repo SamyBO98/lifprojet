1844b42a9ec0559aeaf77b0dc41138af4518ea34
SamyBO98 <samy.ben98@outlook.fr>
2021-10-18 19:38:52 +0200

    transfer from forge.univ-lyon1.fr to github

---
+++ Assets/Editor/CameraFollowEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(CameraFollow))]
+
+public class CameraFollowEditor: Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        CameraFollow cf = (CameraFollow)target;
+        if(GUILayout.Button("Set Min Cam Position"))
+        {
+            cf.SetMinPositon();
+        }
+
+        if (GUILayout.Button("Set Max Cam Position"))
+        {
+            cf.SetMaxPosition();
+        }
+    }
+}
